94cadd07f3f9ef2948a4f55d810dd29f04680ff0
Jocelyn <jocelynvargas123@yahoo.com>
2023-03-08 10:34:23 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards( names, value ) {
+       let messageNames = []
+    for (let i = 0; i < names.length; i++) {
+      messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+return messageNames
+}
+
+
+function countDown(i) {
+    let a = 0
+    while(i >= a) {
+        console.log(i)
+        i--
+    }
+}
+
+countDown()
+
+
+// countDown()
